72d87de17ee55f712fb4fe7d12ce2ca0fbcde469
vijaykumar18 <vijaykumar18@gmail.com>
2024-03-09 12:34:53 +0530

    server script

--- server-script.sh
+++ server-script.sh
@@ -3,16 +3,21 @@
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
 sudo yum install maven -y
-
-if [ -d "addressbook" ]
-then
-   echo "repo already cloned and exists"
-   cd /home/ec2-user/addressbook
-   git checkout test
-   git pull origin test
-else
-   git clone https://github.com/theetla/addressbook.git
-   git checkout test
-fi
+git clone https://github.com/theetla/addressbook.git
 cd addressbook
+git checkout test
 mvn package
+
+
+# if [ -d "addressbook" ]
+# then
+#    echo "repo already cloned and exists"
+#    cd /home/ec2-user/addressbook
+#    git checkout test
+#    git pull origin test
+# else
+#    git clone https://github.com/theetla/addressbook.git
+#    git checkout test
+# fi
+# cd addressbook
+# mvn package 
